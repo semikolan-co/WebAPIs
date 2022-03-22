a25e678e629ef425fedbdb218a0c3e7c84659004
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-03-22 18:04:14 +0530

    Added Properties Description

--- Battery/script.js
+++ Battery/script.js
@@ -1,9 +1,9 @@
-const [level, fill] = document.querySelectorAll('.level, .fill');
-const battery = navigator.getBattery();
+const level = document.querySelector('.level');
+const fill = document.querySelector('.fill');
 const colors = {
-  high: '#59ef0b',
-  normal: '#d76a04',
-  low: '#e70707'
+    high: '#59ef0b',
+    normal: '#d76a04',
+    low: '#e70707'
 }
 
 const updateBattery = batt => {
@@ -13,9 +13,23 @@ const updateBattery = batt => {
     level.dataset.level = fill.style.height = `${~~batt}%`;
   }
 
-battery.then(battery => {
-  
-  
+navigator.getBattery().then(battery => {
+   
+    // This argument passed here is BatteryManager
+    // Here BatteryManager is an reference as `battery`
+    // BatteryManager has 4 Properties
+    // 1. charging - boolean - Indicates whether Battery is currently charging or not
+    // 2. chargingTime - number - Time in seconds until the battery is fully charged (if charging)
+    // 3. dischargingTime - number - Time in seconds until the battery is fully discharged (if not charging)
+    // 4. level - number - Percentage of battery charge (0-1)
+
+    // You can Access these values like this
+    const isCharging = battery.charging;
+    const chargingTime = battery.chargingTime;
+    const dischargingTime = battery.dischargingTime;
+    const level = battery.level;
+
+
   updateBattery(battery.level * 100);
   
   if (battery.dischargingTime == Infinity)
