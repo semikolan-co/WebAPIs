58f9150d7e82da6f616ba8f2c1007b5721d348f9
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-03-22 19:54:01 +0530

    Added Catch Fallback

--- Battery/script.js
+++ Battery/script.js
@@ -28,54 +28,59 @@ const updateChargingStatus = (isCharging, chargingTime, dischargingTime) => {
   }
 };
 
-navigator.getBattery().then((battery) => {
-  // This argument passed here is BatteryManager
-  // Here BatteryManager is an reference as `battery`
-  // BatteryManager has 4 Properties
-  // 1. charging - boolean - Indicates whether Battery is currently charging or not
-  // 2. chargingTime - number - Time in seconds until the battery is fully charged (if charging)
-  // 3. dischargingTime - number - Time in seconds until the battery is fully discharged (if not charging)
-  // 4. level - number - Percentage of battery charge (0-1)
+navigator
+  .getBattery()
+  .then((battery) => {
+    // This argument passed here is BatteryManager
+    // Here BatteryManager is an reference as `battery`
+    // BatteryManager has 4 Properties
+    // 1. charging - boolean - Indicates whether Battery is currently charging or not
+    // 2. chargingTime - number - Time in seconds until the battery is fully charged (if charging)
+    // 3. dischargingTime - number - Time in seconds until the battery is fully discharged (if not charging)
+    // 4. level - number - Percentage of battery charge (0-1)
 
-  // You can Access these values like this
-  const isCharging = battery.charging;
-  const chargingTime = battery.chargingTime;
-  const dischargingTime = battery.dischargingTime;
-  const level = battery.level;
+    // You can Access these values like this
+    const isCharging = battery.charging;
+    const chargingTime = battery.chargingTime;
+    const dischargingTime = battery.dischargingTime;
+    const level = battery.level;
 
-  // Updating Battery Level
-  updateBattery(level * 100);
-  updateChargingStatus(isCharging, chargingTime, dischargingTime);
+    // Updating Battery Level
+    updateBattery(level * 100);
+    updateChargingStatus(isCharging, chargingTime, dischargingTime);
 
-  // if (battery.dischargingTime == Infinity) level.classList.add("power");
+    // if (battery.dischargingTime == Infinity) level.classList.add("power");
 
-  // BatteryManager has 4 Events also
-  // 1. chargingchange - Event fired when the charging status of the battery changes
-  // 2. chargingtimechange - Event fired when the charging time of the battery changes
-  // 3. dischargingtimechange - Event fired when the discharging time of the battery changes
-  // 4. levelchange - Event fired when the battery level changes
+    // BatteryManager has 4 Events also
+    // 1. chargingchange - Event fired when the charging status of the battery changes
+    // 2. chargingtimechange - Event fired when the charging time of the battery changes
+    // 3. dischargingtimechange - Event fired when the discharging time of the battery changes
+    // 4. levelchange - Event fired when the battery level changes
 
-  // Event Listener for chargingchange
-  battery.addEventListener("chargingchange", (b) => {
-    b = b.target;
-    updateChargingStatus(b.charging, b.chargingTime, b.dischargingTime);
+    // Event Listener for chargingchange
+    battery.addEventListener("chargingchange", (b) => {
+      b = b.target;
+      updateChargingStatus(b.charging, b.chargingTime, b.dischargingTime);
+    });
+
+    // Event Listener for levelchange
+    battery.addEventListener("levelchange", (b) => {
+      b = b.target;
+      updateBattery(b.level * 100);
+    });
+
+    // Event Listener for chargingtimechange
+    battery.addEventListener("chargingtimechange", (b) => {
+      b = b.target;
+      updateChargingStatus(b.charging, b.chargingTime, b.dischargingTime);
+    });
+
+    // Event Listener for dischargingtimechange
+    battery.addEventListener("dischargingtimechange", (b) => {
+      b = b.target;
+      updateChargingStatus(b.charging, b.chargingTime, b.dischargingTime);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
-
-  // Event Listener for levelchange
-  battery.addEventListener("levelchange", (b) => {
-    b = b.target;
-    updateBattery(b.level * 100);
-  });
-
-  // Event Listener for chargingtimechange
-  battery.addEventListener("chargingtimechange", (b) => {
-    b = b.target;
-    updateChargingStatus(b.charging, b.chargingTime, b.dischargingTime);
-  });
-
-  // Event Listener for dischargingtimechange
-  battery.addEventListener("dischargingtimechange", (b) => {
-    b = b.target;
-    updateChargingStatus(b.charging, b.chargingTime, b.dischargingTime);
-  });
-});
