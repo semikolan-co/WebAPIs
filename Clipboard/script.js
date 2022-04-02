905d8bce9c8f05da816986ade6e0a248c25df415
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-04-03 00:18:31 +0530

    Added Customized Toast

--- Clipboard/script.js
+++ Clipboard/script.js
@@ -1,43 +1,58 @@
-const copyButton = document.querySelector('#copybutton')
-const pasteButton = document.querySelector('#pastebutton')
-const clearButton = document.querySelector('#clearbutton')
-const pasteTextArea = document.querySelector('#pastetextarea')
-const copyTextArea = document.querySelector('#copytextarea')
+const copyButton = document.querySelector("#copybutton");
+const pasteButton = document.querySelector("#pastebutton");
+const clearButton = document.querySelector("#clearbutton");
+const pasteTextArea = document.querySelector("#pastetextarea");
+const copyTextArea = document.querySelector("#copytextarea");
 
-copyButton.addEventListener('click', () => {
-  const text = copyTextArea.value
-	navigator.clipboard.writeText(text)
-  .then(() => {
-    console.log(`"${text}" was copied to your clipboard.`)
-  })
-  .catch(err => {
-    console.error(`Error copying text to clipboard: ${err}`)
-  })
-})
+copyButton.addEventListener("click", () => {
+  const text = copyTextArea.value;
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      console.log(`"${text}" was copied to your clipboard.`);
+    })
+    .catch((err) => {
+      console.error(`Error copying text to clipboard: ${err}`);
+    });
+});
 
-pasteButton.addEventListener('click', () => {
-	navigator.clipboard.readText()
-  .then(text => {
-    pasteTextArea.value += text;
-  })
-  .catch(err => {
-    console.error(`Error pasting clipboard's content: ${err}`)
-  })
-})
+pasteButton.addEventListener("click", () => {
+  navigator.clipboard
+    .readText()
+    .then((text) => {
+      pasteTextArea.value += text;
+    })
+    .catch((err) => {
+      console.error(`Error pasting clipboard's content: ${err}`);
+    });
+});
 
-clearButton.addEventListener('click', () => {
-  pasteTextArea.value = ''
-})
-
-
+clearButton.addEventListener("click", () => {
+  pasteTextArea.value = "";
+});
 
 document.addEventListener("copy", (event) => {
-  console.log("Text copied to clipboard");
+ showToast("Copied to clipboard!");
 });
 document.addEventListener("paste", (event) => {
-  console.log("Text pasted from clipboard");
-}
-);
-document.addEventListener("cut", (event) => {
-  console.log("Text cut from clipboard");
+  showToast("Pasted from clipboard!");
 });
+document.addEventListener("cut", (event) => {
+  showToast("Cut to clipboard!");
+});
+
+
+const showToast = (text) => {
+  Toastify({
+    text: text,
+    gravity: "bottom", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    style: {
+      background: "linear-gradient(to right, var(--LightSlate), var(--LightestSlate))",
+      color: "var(--Navy)",
+      fontWeight: "bold",
+      fontFamily: "monospace",
+      borderRadius: "5px",
+    }
+  }).showToast();
+}
