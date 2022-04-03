7ca52a076333f374d0ea514990b1217891e466d1
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-04-03 10:39:28 +0530

    Formatted JS Logic

--- Eyedropper/script.js
+++ Eyedropper/script.js
@@ -1,38 +1,38 @@
-
+const btn = document.querySelector(".btn");
+const container = document.querySelector(".container");
+const hexInfo = document.querySelector(".hex-info");
+const body = document.querySelector("body");
+const message = document.createElement("p");
 function showNoSupport() {
-  const $body = document.querySelector('body');
-  const $message = document.createElement('p');
-  $message.classList.add('error');
-  $message.innerHTML = '😕 Your browser does not support the <a href="https://wicg.github.io/eyedropper-api/" target="_blank" rel="noopener">EyeDropper API.<a/>';
-  $body.appendChild($message);
+  message.classList.add("error");
+  message.innerHTML =
+    '😕 Your browser does not support the <a href="" target="_blank" rel="noopener">EyeDropper API.<a/>';
+  body.appendChild(message);
 }
 
 function dropper() {
   const eyeDropper = new EyeDropper();
-  const $btn = document.querySelector('.btn');
-  const $container = document.querySelector('.container');
-  const $hexInfo = document.querySelector('.hex-info');
-  
-  function showResult(hex = '#FFFFFF') {
-    $container.style.backgroundColor = hex;
-    $hexInfo.innerText = hex;
+
+  function showResult(hex = "#FFFFFF") {
+    container.style.backgroundColor = hex;
+    hexInfo.innerText = hex;
   }
-  
+
   function openDropper() {
-    eyeDropper.open()
-      .then(res => {
+    eyeDropper
+      .open()
+      .then((res) => {
         if (res && res.sRGBHex) {
           showResult(res.sRGBHex);
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.error(err);
       });
   }
-  
-  $btn.addEventListener('click', openDropper);
-}
 
+  btn.addEventListener("click", openDropper);
+}
 
 function init() {
   if (window.EyeDropper) {
