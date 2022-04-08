7f96214cd920af05f1fae5fbd93eb7499612fd6f
meinhoonharsh <harshprogrammer782@gmail.com>
2022-04-08 23:26:54 +0530

    Created Logic for Geolocation App

--- Geolocation/script.js
+++ Geolocation/script.js
@@ -1,45 +1,51 @@
-const btn = document.querySelector(".btn");
-const container = document.querySelector(".container");
-const hexInfo = document.querySelector(".hex-info");
-const body = document.querySelector("body");
-const message = document.createElement("p");
-function showNoSupport() {
-  message.classList.add("error");
-  message.innerHTML =
-    '😕 Your browser does not support the <a href="" target="_blank" rel="noopener">EyeDropper API.<a/>';
-  body.appendChild(message);
-}
+const statuss = document.querySelector('#status')
+const latitude = document.querySelector('#latitude')
+const longitude = document.querySelector('#longitude')
+const heading = document.querySelector('#heading')
+const speed = document.querySelector('#speed')
+const altitude = document.querySelector('#altitude')
+const accuracy = document.querySelector('#accuracy')
+const altitudeAccuracy = document.querySelector('#altitudeAccuracy')
+const result = document.querySelector('.results')
+const trigger = document.querySelector('#trigger')
 
-function dropper() {
-  const eyeDropper = new EyeDropper();
 
-  function showResult(hex = "#FFFFFF") {
-    container.style.backgroundColor = hex;
-    hexInfo.innerText = hex;
+function geolocate() {
+
+  const success = (position) => {
+
+    latitude.innerHTML = position.coords.latitude + '&deg;'
+    longitude.innerHTML = position.coords.longitude + '&deg;'
+    heading.innerHTML = position.coords.heading ?? 'N/A'
+    speed.innerHTML = position.coords.speed ?? 'N/A'
+    altitude.innerHTML = position.coords.altitude ?? 'N/A'
+    accuracy.innerHTML = (position.coords.accuracy / 1609).toFixed(3) + ' miles'
+    altitudeAccuracy.innerHTML = (position.coords.altitudeAccuracy / 1609).toFixed(3) + ' miles'
+
+    statuss.innerHTML = ''
+    result.style.display = 'flex'
+    statuss.style.display = 'none'
+    trigger.innerHTML = 'Refresh location'
+    console.log(position)
   }
-
-  function openDropper() {
-    eyeDropper
-      .open()
-      .then((res) => {
-        if (res && res.sRGBHex) {
-          showResult(res.sRGBHex);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+  const error = () => {
+    statuss.innerHTML = 'Unable to retrieve your location.'
   }
-
-  btn.addEventListener("click", openDropper);
-}
-
-function init() {
-  if (window.EyeDropper) {
-    dropper();
+  if (navigator.geolocation) {
+    statuss.innerHTML = 'Locating...'
+    trigger.innerHTML = 'Locating...'
+    navigator.geolocation.getCurrentPosition(success, error)
   } else {
-    showNoSupport();
+    trigger.innerHTML = 'Not supported.'
+    statuss.innerHTML = 'Geolocation is not supported by your browser.'
+    statuss.style.display = 'block'
+
   }
+
+
+
 }
 
-init();
+trigger.addEventListener('click', () => {
+  geolocate()
+})
